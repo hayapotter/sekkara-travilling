32744067657d1a4c362b87a3ea8bebc248ef3c7a
hayapotter <135427290+hayapotter@users.noreply.github.com>
2023-06-16 23:58:00 +0300

    new update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let x =parseInt(prompt("Please Enter the x value"));
+let y=4;
+//document.write(4**6)
+document.getElementById("mydiv").innerHTML=4**6;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+//window.alert("any thing")
